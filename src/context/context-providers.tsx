07f54cb91ddbabf9c1b676e80c447f064667f9da
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-24 11:06:01 +0300

    remove posthog

--- src/context/context-providers.tsx
+++ src/context/context-providers.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { ThemeProvider } from "@/components/theme-provider";
-import { PostHogProvider } from "./posthog-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "./auth-context";
 import PopulatedReplykeProvider from "./populated-replyke-provider";
@@ -11,18 +10,16 @@ function ContextProviders({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Toaster />
-      <PostHogProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <AuthProvider>
-            <PopulatedReplykeProvider>{children}</PopulatedReplykeProvider>
-          </AuthProvider>
-        </ThemeProvider>
-      </PostHogProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <AuthProvider>
+          <PopulatedReplykeProvider>{children}</PopulatedReplykeProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </>
   );
 }
